b966aaf0637c480e7808b316145345a2bca0cb3b
JCCoello <juancarlos.coello@applydigital.com>
2024-03-08 17:59:23 -0300

    Update

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,9 +1,35 @@
 import { test } from '@playwright/test'
 
-test('the first test', ({ page }) => {
-    page.goto('')
+test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:4200/')
+})
+
+test.describe('suite1', () => {
+    test.beforeEach(async ({ page }) => { //these are hooks that only run for the tests in this suite
+        await page.getByText('Forms').click()
+    })
+
+    test('the first test', async ({ page }) => {
+        await page.getByText('Form Layouts').click()
+    })
+
+    test('navigate to datepicker page', async ({ page }) => {
+        await page.getByText('Datepicker').click()
+    })
 
 })
 
+test.describe.only('suite1', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.getByText('Auth').click()
+    })
 
+    test('the first test1', async ({ page }) => {
+        await page.getByText('Login').click()
+    })
 
+    test('navigate to datepicker page1', async ({ page }) => {
+        await page.getByText('Modal & Overlays').click()
+    })
+
+})
