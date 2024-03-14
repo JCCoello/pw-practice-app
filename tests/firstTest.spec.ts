f0b0de1496f4d467cfa57df1116bbe8ac7fcbba6
JCCoello <juancarlos.coello@applydigital.com>
2024-03-14 17:59:38 -0300

    Update

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,4 +1,6 @@
-import { test } from '@playwright/test'
+import { test, expect } from '@playwright/test'
+import { EmptyError } from 'rxjs'
+
 
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/')
@@ -6,32 +8,114 @@ test.beforeEach(async ({ page }) => {
     await page.getByText('Form Layouts').click()
 })
 
-test('Locator syntax rules', async ({ page }) => { //In the following example we are trying to pick the email input fiels
-    // By tag name
-    page.locator('input', {})   //The most common form of finding the loactor is "Locator" in the case the first '' includes the string for the locator and the {} is the options for the locator
-    //PW will return all the web elements that heve "input" and the second element narrows dows what it will return
-    // By ID
-    page.locator('#inputEmail1') //In this case if you want to find the element that has the "id:" you just type #
+test('User facing Locators', async ({ page }) => {
+    await page.getByRole('textbox', { name: "Email" }).first().click() //In this case it is pointing to the  textbox that has the name "Email" but there are two elements with the same name so we make it click on the .first element
+    await page.getByRole('button', { name: "Sign in" }).first().click() //This does the same for the Sign in button
 
-    // By class value
-    page.locator('.shape-rectangle') //In this case, when chosing al enement by class, you first call it by putting a "." in font and then one of the elements that are within the class element in the inspector
+    await page.getByLabel('Email').first().click() //This is another way of getting the emelent by label
 
-    // By attribute
-    page.locator('[placeholder="Email"]') // Yo find any element by its attribute you put "[]" and place the element with its value
+    await page.getByPlaceholder('Jane Doe').click()
 
-    // By FULL class value
-    page.locator('class="input-full-width size-medium status-basic shape-rectangle nb-transition"') // This is similar to the one above, but this is more complete than usung the dot notation
+    await page.getByText('Using the Grid').click()
 
-    // Combine different selectors
-    page.locator('input[placeholder="Email"].shape-rectangle') // In this case you can narrow down even more to find exactly what you want to click on
+    await page.getByTestId('signIn').click()    //In this case the tag is something that the user cant see but it makes for better test, to search for this ID, you have to search within
+    //the source code by searching in VS, in this case you click on the magnifiyng glass and search for "Sign in"
+    //then you add in the source code at the sign in button after the "<button" add "data-testid="signIn"" befire the "type"
 
-    //By XPath (NOT RECOMMENDED)
-    page.locator('//*[@id="inputEmail1"]') //This is using the XPath but is not recommended
+    await page.getByTitle('IoT Dashboard').click()
+})
 
-    //By partial text match
-    page.locator(':text("Using")') //This uses part of the text on the page
+test('locating child elements', async ({ page }) => {
+    await page.locator('nb-card nb-radio :text-is("Option 1")').click() //You can run it like this (Side by side) or chaining the things (like below)
+    await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 2")').click()
 
-    //By exact text match
-    page.locator(':text-is("Using the Grid")') //This uses exact text on the page
+    await page.locator('nb-card').getByRole('button', { name: "Sign in" }).first().click() //This is another way of clicking on the Sign in button, in this case "nb-card" is not needed, just for your reference, bc we clickedd on it earlier without using it
+
+    await page.locator('nb-card').nth(5).getByRole('button').click() //This method is not a preffered way of getting the element, it gets the 6th element that has the "button" role  (The 2nd sign in button). Use this method as a last result
+})
+
+test('locating parent elements', async ({ page }) => {
+    // Here we are gonna locate a particular Email text box as there are many Email forms, this will be done by the text in the title that the box contains
+    //  If we want to tocate the email field within the box "Using the grid" we do it this way:
+    // We inspect the website and get the parent of the "np-card-header" thet has the text "Using the Grid"
+
+    await page.locator('nb-card', { hasText: "Using the Grid" }).getByRole('textbox', { name: "Email" }).click()
+    // In this case only one element will be returned, differing what was returned in the previous lesson and then find the textbox that contains "email" as TEXT
+
+    await page.locator('nb-card', { has: page.locator('#inputPassword2') }).getByRole('textbox', { name: "Password" }).click()
+    // In this case we nestled a locator within the parent element and we click on the pw field within the "Using the Grid" element
+
+    await page.locator('nb-card').filter({ hasText: "Basic Form" }).getByRole('textbox', { name: "Email" }).click()
+    // In this case we are filtering within the nb-card within the card "Basic Form"
+
+    await page.locator('nb-card').filter({ has: page.locator('.status-danger') }).getByRole('textbox', { name: "Password" }).click()
+    // In this case we are identifying the card to click on the element by the color of the button
+
+    await page.locator('nb-card').filter({ has: page.locator('nb-checkbox') }).filter({ hasText: "Sign in" }).getByRole('textbox', { name: "Email" }).click()
+    // Here we are finding the email element by filtering the elements that have a "Checkbox" and a "Sign in" button
+
+    await page.locator(':text-is("Using the Grid').locator('..').getByRole('textbox', { name: "Email" }).click()
+    // The ".." notation brings a level up to the element. This apparently is the XPath method
+
+})
+
+test('Reusing the locators', async ({ page }) => { // Here we are gonna automate a login and the refactor to use less code
+    //await page.locator('nb-card').filter({ hasText: "Basic Form" }).getByRole('textbox', { name: "Email" }).fill('juan@test.com')
+    //await page.locator('nb-card').filter({ hasText: "Basic Form" }).getByRole('textbox', { name: "Password" }).fill('AssworDXX123?¡')
+    //await page.locator('nb-card').filter({ hasText: "Basic Form" }).getByRole('button').click()
+    //Now we rewrite the code to simplify by putting duplicated code into a "const" like thsi:
+
+
+    const basicForm = page.locator('nb-card').filter({ hasText: "Basic Form" })
+    const emailField = basicForm.getByRole('textbox', { name: "Email" }) // Note that in this case we used a const inside a const in order to print the email, making it much shorter
+
+    await emailField.fill('juan@test.com')
+    await basicForm.getByRole('textbox', { name: "Password" }).fill('AssworDXX123?¡')
+    await basicForm.locator('nb-checkbox').click()
+    await basicForm.getByRole('button').click()
+
+    await expect(emailField).toHaveValue('juan@test.com')
+    // Here we wrote our first assertion, be aware that we inported the "expect" thing at the beggining of the test
+
+})
+
+test('Extracting valus', async ({ page }) => { // Here we want to validate that the "submit" button has the text "submit" on the "Basic Form" element
+    // Single text value
+    const basicForm = page.locator('nb-card').filter({ hasText: "Basic Form" })
+    const buttonText = await basicForm.locator('button').textContent()
+    expect(buttonText).toEqual('Submit')
+
+    // All text values
+    // Here we are gonna take the radio button under "Using the Grid" and verify at least one radio button has the value of "Option 1"
+    const allRadioButtonLabels = await page.locator('nb-radio').allTextContents()
+    expect(allRadioButtonLabels).toContain("Option 1")
+
+    // Input value 
+    const emailField = basicForm.getByRole('textbox', { name: "Email" })
+    await emailField.fill('test@test.com')
+    const emailValue = await emailField.inputValue()
+    expect(emailValue).toEqual('test@test.com')
+
+    const placeholderVaule = await emailField.getAttribute('placeholder')
+    expect(placeholderVaule).toEqual('Email')
+})
+
+test('Assertions', async ({ page }) => {
+
+    const basicFormButton = page.locator('nb-card').filter({ hasText: "Basic Form" }).locator('button')
+
+    //General assertions
+    const value = 5
+    expect(value).toEqual(5) // In this very simple example, on the left you chose the element to find and on the right the value of this element (Exact value), if you notice in this assertion there is no wait
+
+    //Locator assertion
+    const text = await basicFormButton.textContent()
+    expect(text).toEqual("Submit")
+
+    await expect(basicFormButton).toHaveText('Submit') //BTW when you put an "await" before, it will wait until 5 seconds before it gives up, different to the General assertions which will not wait
+
+    // Soft assertion
+    await expect.soft(basicFormButton).toHaveText('SubmitXXX')
+    await basicFormButton.click()
 
 })
