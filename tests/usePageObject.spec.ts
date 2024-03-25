c7a9aa8c298a944e847e9d73d381c26287b75d7e
JCCoello <juancarlos.coello@applydigital.com>
2024-03-25 15:42:59 -0300

    update

---
+++ tests/usePageObject.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test'
+
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:4200/')
+})
+
+test('Navigate to form page', async ({ page }) => {
+
+
+})
